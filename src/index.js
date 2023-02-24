90d9d0d8980df52fe8ae8674e165b20fce721949
moslem <moslemhosseinpour1998@gmail.com>
2023-02-24 20:51:40 +0330

    add-router

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
-const express = require("express");
-const userRouter = require("./routers/user");
-require("./db/mongoose");
+// const express = require("express");
+// const userRouter = require("./routers/user");
+// require("./db/mongoose");
 
-const app = express();
-const port = process.env.PORT || 3000;
+// const app = express();
+// const port = process.env.PORT || 3000;
 
-app.use(express.json());
-app.use(userRouter);
+// app.use(express.json());
+// app.use(userRouter);
 
-app.listen(port, () => {
-  console.log("Server is up on port" + port);
-});
+// app.listen(port, () => {
+//   console.log("Server is up on port" + port);
+// });
+
+const bcrypt = require("bcryptjs");
+
+myFunc = async () => {
+  const password = "moslem";
+  const hashPassword = await bcrypt.hash(password, 8);
+  console.log(hashPassword);
+  const isMatch = await bcrypt.compare(password, hashPassword);
+  console.log(isMatch);
+};
+myFunc();
